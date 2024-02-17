76d2fb6ef926b5795b85a21545525042f416b932
Robert <robert.gurde@gmail.com>
2024-02-17 13:40:47 +0000

    Refactor

--- bin/ups.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Expects to be launched in the virgo ups project dir (usually: /usr/share/virgo/)
-/usr/bin/python3 -m virgo.ups.service
---
+++ src/bin/ups.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Expects to be launched in the virgo ups project dir (usually: /usr/share/virgo/ups/)
+/usr/bin/python3 -m ups.service
