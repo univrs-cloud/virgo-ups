204ca8364c5491530569006e6808e61218145669
Robert <36549+VeloAddict@users.noreply.github.com>
2024-09-06 15:18:05 +0300

    Monkeypatch gpiochip number on RPi5 to 0
    A recent change in the RPi kernel switched the gpiochip number on RPi5 to 0 (see raspberrypi/linux#6144) but gpiozero still uses a hardcoded chip number of 4 on RPi5 (see https://github.com/gpiozero/gpiozero/blob/master/gpiozero/pins/lgpio.py#L66) and thus fails to find the GPIO pin

--- src/ups/service.py
+++ src/ups/service.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+import gpiozero.pins.lgpio
+import lgpio
+
+def __patched_init(self, chip=None):
+    gpiozero.pins.lgpio.LGPIOFactory.__bases__[0].__init__(self)
+    chip = 0
+    self._handle = lgpio.gpiochip_open(chip)
+    self._chip = chip
+    self.pin_class = gpiozero.pins.lgpio.LGPIOPin
+
+gpiozero.pins.lgpio.LGPIOFactory.__init__ = __patched_init
+
 import logging, platform, sys
 import smbus
 
